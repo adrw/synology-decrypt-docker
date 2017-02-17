cda58e18d3814531fe575a327ce08c5507ec67f7
Andrew Paradi <me@andrewparadi.com>
2017-02-17 11:52:37 -0500

    example encrypted files and updated to use 64bit decrypt tool

--- startup.sh
+++ startup.sh
@@ -8,8 +8,8 @@ mkdir -p /var/run/sshd
 
 mkdir ~/syno
 cd ~/syno
-wget http://global.download.synology.com/download/Tools/SynologyCloudSyncDecryptionTool/013/Linux/i686/SynologyCloudSyncDecryptionTool-013.tar.gz
-tar xvzf SynologyCloudSyncDecryptionTool-013.tar.gz
+wget http://global.download.synology.com/download/Tools/SynologyCloudSyncDecryptionTool/013/Linux/i686/SynologyCloudSyncDecryptionTool-013_x64.tar.gz
+tar xvzf SynologyCloudSyncDecryptionTool-013_x64.tar.gz
 bash -c 'echo "~/syno/SynologyCloudSyncDecryptionTool" >> /.bashrc'
 
 # start a shell
